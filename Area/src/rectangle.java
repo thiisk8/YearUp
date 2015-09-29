6f5ee0e90861c4e7966794b1af4d90f5d75300b5
thiisk8 <tdeoliveira@atl.yearup.org>
2015-09-29 10:43:19 -0500

    Fire

---
+++ Area/src/rectangle.java
@@ -0,0 +1,20 @@
+
+public class rectangle {
+public static void main(String args[]){
+	double c = ac(5);
+			double r = ar(3,2);
+	System.out.println("Area of the circle is  " + c + " " + "Area of the rectangle is" + r);
+	
+}
+public static double ac(double radius){
+	
+	double ac =3.14*radius*radius;
+	return ac;
+}
+
+public static double ar(double length, double width){
+	
+	double ar =width*length;
+			return ar;
+}
+}
