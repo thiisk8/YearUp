910eee12fec0c4e5203d43e816409e009a4495f5
thiisk8 <tdeoliveira@atl.yearup.org>
2015-09-30 12:46:34 -0500

    I'm commiting

---
+++ Intro/src/Intro.java
@@ -0,0 +1,12 @@
+
+public class Intro {
+
+public static void main(String[] args)
+{
+	
+System.out.println("Thiago de Oliveira");	
+System.out.println("My favorite book is Harry Potter and the Prisoner of Azkaban");
+System.out.println("I am awesome.");
+}
+
+}
